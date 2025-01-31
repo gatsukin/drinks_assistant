d2d39c70eb3dfe5d6b7239b6ea6280a30a7a09d4
zhdanov konstantin <andreewwhite@gmail.com>
2025-01-31 09:45:46 +0700

    update bot for error catch

--- bot/app.js
+++ bot/app.js
@@ -1,17 +1,30 @@
-import { Telegraf, Markup } from 'telegraf'
+const { Telegraf, Markup } = require('telegraf');
 
-const token = '7693523071:AAHxz45NAWMAdC0qiJcANGBU2sKmtfK0UlY'
-const webAppUrl = 'https://drinks-assistant.netlify.app/'
+const token = '7693523071:AAHxz45NAWMAdC0qiJcANGBU2sKmtfK0UlY';
+const webAppUrl = 'https://drinks-assistant.netlify.app/';
 
-const bot = new Telegraf(token)
+const bot = new Telegraf(token);
 
-bot.command('start', (ctx) => {
-  ctx.reply(
-    'Нажми, чтобы запустить приложение',
-    Markup.inlineKeyboard([
-      Markup.button.webApp('Открыть DrinkAssistant', `${webAppUrl}`),
-    ])
-  )
-})
+bot.command('start', async (ctx) => {
+  try {
+    await ctx.reply(
+      'Нажми, чтобы запустить приложение',
+      Markup.inlineKeyboard([
+        Markup.button.webApp('Открыть DrinkAssistant', webAppUrl),
+      ])
+    );
+  } catch (error) {
+    console.error('Ошибка при отправке:', error.message);
+  }
+});
 
-bot.launch()
+// Обработка глобальных ошибок
+bot.catch((err, ctx) => {
+  console.error(`Ошибка в обновлении ${ctx.update.update_id}:`, err);
+});
+
+bot.launch();
+
+// Корректное завершение работы
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
