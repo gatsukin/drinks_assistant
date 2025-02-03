0dc1fbefb9e49238c916d84da5bf5ec794eca6ce
zhdanov konstantin <andreewwhite@gmail.com>
2025-02-03 09:51:04 +0700

    disabled bar adding to friends bar

--- src/components/Bar/BarFilterButtons.vue
+++ src/components/Bar/BarFilterButtons.vue
@@ -2,7 +2,12 @@
     <VBtn @click="$emit('toggleFilter')">
         <VIcon :size="20" icon="ri-filter-line" />
     </VBtn>
-    <RouterLink to="/bar/add" custom v-slot="{ isActive, navigate }">
+    <RouterLink
+        to="/bar/add"
+        custom
+        v-slot="{ isActive, navigate }"
+        v-if="user.telegram == user.bar_id"
+    >
         <VBtn @click="navigate" :class="{ active: isActive }">
             <VIcon :size="20" icon="ri-add-line" />
         </VBtn>
@@ -10,5 +15,8 @@
 </template>
 
 <script setup>
+import { useAppStore } from "@/stores/app";
+const { user } = useAppStore();
+
 defineEmits(["toggleFilter"]);
 </script>
